016c9dc2c9707d019d470385183c827b587bb23e
alo40 <alejandronieto40@gmail.com>
2022-01-30 22:06:36 +0100

    add main code

--- SAT_score.py
+++ SAT_score.py
@@ -1,1 +1,14 @@
 ## SAT score question in U1 L2
+
+from scipy.stats import bernoulli
+from random import choices
+from numpy import mean
+import matplotlib.pyplot as plt
+
+a = 25
+b = -5
+p = 1/6
+size = 40
+X = bernoulli(p).rvs(size)
+S = a*X + b
+print("finish")
