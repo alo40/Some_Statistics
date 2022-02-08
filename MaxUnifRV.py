3a984b552210296894aae6ad241db78abde4d6fd
alo40 <alejandronieto40@gmail.com>
2022-02-08 21:05:19 +0100

    added code

--- MaxUnifRV.py
+++ MaxUnifRV.py
@@ -1,1 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
+def f(x):
+    return x**2*np.exp(-x**2)
+
+x = np.linspace ( start = 0.    # lower limit
+                , stop = 3      # upper limit
+                , num = 51      # generate 51 points between 0 and 3
+                )
+y = f(x)    # This is already vectorized, that is, y will be a vector!
+plt.plot(x, y)
+plt.show()
