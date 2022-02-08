9d7be919a8b59a2c3a0a54fe6859c1e89502ddf1
alo40 <alejandronieto40@gmail.com>
2022-02-08 22:34:27 +0100

    save last status

--- MaxUnifRV.py
+++ MaxUnifRV.py
@@ -11,6 +11,10 @@ def pdf(x, n, a, b):
 def cdf(x, n, a, b):
     return ((x-a)/(b-a))**n # CFD of X_n
 
+## pdf of the limit function
+def pdf_limit(x, n, a, b):
+    return n*(1-n*(x-a)**(n-b)/(b-a)**n)
+
 ## parameters
 a = 0 # lower limit of Unif. distribution
 b = 1 # upper limit of Unif. distribution
@@ -19,9 +23,11 @@ x = np.linspace ( 0., 1, 101)
 
 ## plot
 y1 = pdf(x, n, a, b)
-y2 = cdf(x, n, a, b)
+# y2 = cdf(x, n, a, b)
+# y3 = pdf_limit(x, n, a, b)
 plt.plot(x, y1, 'r') # pdf plot
-plt.plot(x, y2, 'b') # cdf plot
+# plt.plot(x, y2, 'b') # cdf plot
+# plt.plot(x, y3, 'g') # pdf_limit plot
 plt.show()
 
 ## Some not used verbatim
